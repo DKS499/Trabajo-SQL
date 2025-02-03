b529ee0048d3f9742d30d439111407f026740399
Víctor Marín Escribano <victor.marin4@educa.madrid.org>
2025-02-03 20:49:50 +0100

    añado las preguntas

--- consultas DDL.sql
+++ consultas DDL.sql
@@ -1,1 +1,18 @@
 USE aeropuerto;
+/*selecciona el dni de los pasajeros que vuelan con el empleado El Richarz*/
+
+-- selecciona los nombres de los pilotos que han tenido como copiloto a Matfied
+
+-- selecciona toda la informacion de los vuelos no internacionales
+
+-- cuenta el total de dinero gastado en los sueldos anualmente
+
+-- selecciona los dnis de los pasajeros en los vuelos con codigo 1 y 3
+
+-- selecciona los empleados con sueldos mayores a 48000
+
+-- cuenta cuantos pilotos hay en la plantilla
+
+-- selecciona al empleado con mayor salario
+
+-- selecciona a todos los empleados que tengan mas de seis meses de antigüedad
